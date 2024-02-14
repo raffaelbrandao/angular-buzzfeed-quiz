4600eb899e2cc7aaa196ed860499d321a5030766
Rafael Brandão <raffaelbrandao@gmail.com>
2024-02-14 17:47:38 -0300

    Update quiz logic

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quiz_questions.json';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.css'
+  styleUrl: './quiz.component.css',
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  title: string = '';
+  questions: any;
+  questionSelected: any;
+  answers: string[] = [];
+  answerSelected: string = '';
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+  finished: boolean = false;
 
+  constructor() {}
+  ngOnInit(): void {
+    if (quiz_questions) {
+      this.finished = false;
+      this.title = quiz_questions.title;
+      this.questions = quiz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  answerChoose(alias: string) {
+    this.answers.push(alias);
+    this.nextStep();
+  }
+
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const aliasMore: string = await this.checkResult(this.answers);
+      this.finished = true;
+      this.answerSelected =
+        quiz_questions.results[
+          aliasMore as keyof typeof quiz_questions.results
+        ];
+    }
+  }
+
+  async checkResult(answers: string[]) {
+    const result = answers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
 }
